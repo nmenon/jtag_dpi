91d8123a90d7b4bd9114f45a97179b69c27b3a2b
Nishanth Menon <nm@ti.com>
2017-07-01 04:07:31 -0500

    jtag_dpi: basic changes and formatting
    Some ideas from
    https://github.com/rdiez/jtag_dpi/blob/master/jtag_dpi.cpp
    
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- src/dpi/jtag_dpi.c
+++ src/dpi/jtag_dpi.c
@@ -1,5 +1,3 @@
-#include "svdpi.h"
-
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,25 +5,24 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <fcntl.h>
+#include  <string.h>
 #include "dpiheader.h"
 
-extern svLogic rtl_get_tdo();
-
-int jp_check_con();
 
 uint8_t jp_waiting;
 uint8_t count_comp;
 uint8_t jp_got_con;
 
-static int jp_comm_m;		// The listening socket
-static int jp_comm;		// The socket for communicating with jp
+static int jp_server_p;		// The listening socket
+static int jp_client_p;		// The socket for communicating with Remote
 
 int socket_port;
 
-void socket_open()
+static int server_socket_open()
 {
 	struct sockaddr_in addr;
 	int ret;
+	int yes = 1;
 
 	count_comp = 0;
 	jp_waiting = 0;
@@ -36,84 +33,76 @@ void socket_open()
 	addr.sin_addr.s_addr = INADDR_ANY;
 	memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
 
-	jp_comm_m = socket(PF_INET, SOCK_STREAM, 0);
-	if (jp_comm_m < 0) {
+	jp_server_p = socket(PF_INET, SOCK_STREAM, 0);
+	if (jp_server_p < 0) {
 		fprintf(stderr, "Unable to create comm socket: %s\n",
 			strerror(errno));
-		return;
+		return errno;
 	}
 
-	int yes = 1;
-	if (setsockopt(jp_comm_m, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))
+	if (setsockopt(jp_server_p, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int))
 	    == -1) {
 		fprintf(stderr, "Unable to setsockopt on the socket: %s\n",
 			strerror(errno));
-		return;
+		return -1;
 	}
 
-	if (bind(jp_comm_m, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+	if (bind(jp_server_p, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
 		fprintf(stderr, "Unable to bind the socket: %s\n",
 			strerror(errno));
-		return;
+		return -1;
 	}
 
-	if (listen(jp_comm_m, 1) == -1) {
+	if (listen(jp_server_p, 1) == -1) {
 		fprintf(stderr, "Unable to listen: %s\n", strerror(errno));
-		return;
+		return -1;
 	}
 
-	ret = fcntl(jp_comm_m, F_GETFL);
+	ret = fcntl(jp_server_p, F_GETFL);
 	ret |= O_NONBLOCK;
-	fcntl(jp_comm_m, F_SETFL, ret);
+	fcntl(jp_server_p, F_SETFL, ret);
 
 	fprintf(stderr, "Listening on port %d\n", socket_port);
+	return 0;
 }
 
-int jtag_recv(svLogic * tck, svLogic * trstn, svLogic * tdi, svLogic * tms)
+static int client_recv(unsigned char *const jtag_tms,
+		unsigned char *const jtag_tck,
+		unsigned char *const jtag_trst,
+		unsigned char *const jtag_tdi,
+		unsigned char *const jtag_new_data_available,
+		const unsigned char jtag_tdo)
 {
-	uint8_t tdo;
 	uint8_t dat;
 	int ret;
 
-	if (!jp_got_con) {
-		if (!jp_check_con()) {
-			return 0;
-		}
-	}
-
-	ret = recv(jp_comm, &dat, 1, 0);
+	ret = recv(jp_client_p, &dat, 1, 0);
 
 	// check connection abort
 	if ((ret == -1 && errno != EWOULDBLOCK) || (ret == 0)) {
 		printf("JTAG Connection closed\n");
 
-		close(jp_comm);
-		socket_open();
-		return 0;
+		close(jp_client_p);
+		*jtag_new_data_available = 0;
+		return server_socket_open();
 	}
 	// no available data
-	if (ret == -1 && errno == EWOULDBLOCK)
+	if (ret == -1 && errno == EWOULDBLOCK) {
+
+		*jtag_new_data_available = 0;
 		return 0;
+	}
 
 	if (dat & 0x80) {
 		switch (dat & 0x7f) {
 		case 0:
-			// jp wants the TDO
-			tdo = rtl_get_tdo();
-
-			// convert from logic to 0/1
-			if (tdo == sv_1)
-				tdo = 1;
-			else
-				tdo = 0;
-
-			send(jp_comm, &tdo, 1, 0);
+			send(jp_client_p, &jtag_tdo, 1, 0);
 			return 0;
 		case 1:
 			// jp wants a time-out
 			if (count_comp) {
 				dat = 0xFF;	// A value of 0xFF is expected, but not required
-				send(jp_comm, &dat, 1, 0);
+				send(jp_client_p, &dat, 1, 0);
 			} else {
 				jp_waiting = 1;
 			}
@@ -121,62 +110,79 @@ int jtag_recv(svLogic * tck, svLogic * trstn, svLogic * tdi, svLogic * tms)
 		}
 	}
 
-	*tck = (dat & 0x1) >> 0;
-	*trstn = (dat & 0x2) >> 1;
-	*tdi = (dat & 0x4) >> 2;
-	*tms = (dat & 0x8) >> 3;
+	*jtag_tck = (dat & 0x1) >> 0;
+	*jtag_trst = (dat & 0x2) >> 1;
+	*jtag_tdi = (dat & 0x4) >> 2;
+	*jtag_tms = (dat & 0x8) >> 3;
 
 	dat |= 0x10;
-	ret = send(jp_comm, &dat, 1, 0);
+	ret = send(jp_client_p, &dat, 1, 0);
 
-	return 1;
+	return 0;
 }
 
+#if 0
 void jtag_timeout()
 {
 	uint8_t dat = 0xFF;
 	if (jp_waiting) {
-		send(jp_comm, &dat, 1, 0);
+		send(jp_client_p, &dat, 1, 0);
 		jp_waiting = 0;
 	}
 
 	count_comp = 1;
 }
-
-void jtag_init(const int port)
-{
-	count_comp = 0;
-	jp_waiting = 0;
-	jp_got_con = 0;
-
-	socket_port = port;
-
-	socket_open();
-}
+#endif
 
 // Checks to see if we got a connection
-int jp_check_con()
+static int client_check_con()
 {
 	int ret;
 
-	if ((jp_comm = accept(jp_comm_m, NULL, NULL)) == -1) {
+	if ((jp_client_p = accept(jp_server_p, NULL, NULL)) == -1) {
 		if (errno == EAGAIN)
 			return 0;
 
 		fprintf(stderr, "Unable to accept connection: %s\n",
 			strerror(errno));
-		return 0;
+		return 1;
 	}
-
 	// Set the comm socket to non-blocking.
+	ret = fcntl(jp_client_p, F_GETFL);
+	ret |= O_NONBLOCK;
+	fcntl(jp_client_p, F_SETFL, ret);
 	// Close the server socket, so that the port can be taken again
 	// if the simulator is reset.
-	ret = fcntl(jp_comm, F_GETFL);
-	ret |= O_NONBLOCK;
-	fcntl(jp_comm, F_SETFL, ret);
-	close(jp_comm_m);
+	close(jp_server_p);
 
 	printf("JTAG communication connected!\n");
 	jp_got_con = 1;
-	return 1;
+	return 0;
 }
+
+int jtag_init(const int port)
+{
+	socket_port = port;
+
+	return server_socket_open();
+}
+
+int jtag_dpi_tick(unsigned char *const jtag_tms,
+		  unsigned char *const jtag_tck,
+		  unsigned char *const jtag_trst,
+		  unsigned char *const jtag_tdi,
+		  unsigned char *const jtag_new_data_available,
+		  const unsigned char jtag_tdo)
+{
+
+	if (!jp_got_con) {
+		if (client_check_con()) {
+			return 0;
+		}
+	}
+
+	return client_recv(jtag_tms, jtag_tck, jtag_trst, jtag_tdi,
+			   jtag_new_data_available, jtag_tdo);
+}
+
+/* vim: set ai: ts=8 sw=8 noet: */
